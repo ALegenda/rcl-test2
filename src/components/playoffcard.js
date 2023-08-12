41203280106716e10b9ea77c180095579c844e6e
TomKuper <adalekseev@edu.hse.ru>
2023-08-13 09:13:17 +1000

    playoff fix

--- src/components/playoffcard.js
+++ src/components/playoffcard.js
@@ -3,19 +3,24 @@ import Link from "./link";
 
 export default class PlayoffCard extends React.Component {
     render() {
-        return (<div class="playoffcard-wrapper">
-            <div class="playoff-teams">
-                <div class="playoff-team">
-                    <a href={"/team/" + this.props.team1Id}><img class="playoff-logo-team" src={this.props.team1Logo} alt="logo"></img></a>
-                    {this.props.team1Name}</div>
-                <div class="playoff-team">
-                    <a href={"/team/" + this.props.team2Id}><img class="playoff-logo-team" src={this.props.team2Logo} alt="logo"></img></a>
-                    {this.props.team2Name}</div>
+        let date = new Date(this.props.time);
+        return (<a href={"/match/" + this.props.matchId}>
+            <div class="playoffcard-wrapper">
+            <div class="playoff-inner">
+                <div class="playoff-teams">
+                    <div class="playoff-team">
+                        <a href={"/team/" + this.props.team1Id}><img class="playoff-logo-team" src={this.props.team1Logo} alt="logo"></img></a>
+                        {this.props.team1Name}</div>
+                    <div class="playoff-team">
+                        <a href={"/team/" + this.props.team2Id}><img class="playoff-logo-team" src={this.props.team2Logo} alt="logo"></img></a>
+                        {this.props.team2Name}</div>
+                </div>
+                <div class="playoff-scores">
+                    <div class="playoff-score">{this.props.team1Score}</div>
+                    <div class="playoff-score">{this.props.team2Score}</div>
+                </div>
             </div>
-            <div class="playoff-scores">
-                <div class="playoff-score">{this.props.team1Score}</div>
-                <div class="playoff-score">{this.props.team2Score}</div>
-            </div>
-        </div>)
+            <div class="playoff-time">{("0" + date.getDay().toString()).slice(-2) + "." + ("0" + date.getMonth().toString()).slice(-2) + " " + ("0" + date.getHours().toString()).slice(-2) + ":" + ("0" + date.getMinutes().toString()).slice(-2)}</div>
+        </div></a>)
     }
 }
--- src/pages/playoff.js
+++ src/pages/playoff.js
@@ -40,33 +40,33 @@ function Playoff() {
                         <div class="playoff-group" id="group7">
                             <div class="playoff-group" id="group3">
                                 <div class="playoff-group" id="group1">
-                                    <PlayoffCard team1Score={playoff[0].team1Score} team2Score={playoff[0].team2Score} team1Logo={playoff[0].team1.logo} team2Logo={playoff[0].team2.logo} team1Name={playoff[0].team1.name} team2Name={playoff[0].team2.name} team1Id={playoff[0].team1.id} team2Id={playoff[0].team2.id} />
-                                    <PlayoffCard team1Score={playoff[1].team1Score} team2Score={playoff[1].team2Score} team1Logo={playoff[1].team1.logo} team2Logo={playoff[1].team2.logo} team1Name={playoff[1].team1.name} team2Name={playoff[1].team2.name} team1Id={playoff[1].team1.id} team2Id={playoff[1].team2.id} />
+                                    <PlayoffCard time={playoff[0].startedAt} matchId={playoff[0].id} team1Score={playoff[0].team1Score} team2Score={playoff[0].team2Score} team1Logo={playoff[0].team1.logo} team2Logo={playoff[0].team2.logo} team1Name={playoff[0].team1.name} team2Name={playoff[0].team2.name} team1Id={playoff[0].team1.id} team2Id={playoff[0].team2.id} />
+                                    <PlayoffCard time={playoff[1].startedAt} matchId={playoff[1].id} team1Score={playoff[1].team1Score} team2Score={playoff[1].team2Score} team1Logo={playoff[1].team1.logo} team2Logo={playoff[1].team2.logo} team1Name={playoff[1].team1.name} team2Name={playoff[1].team2.name} team1Id={playoff[1].team1.id} team2Id={playoff[1].team2.id} />
                                 </div>
                                 <Bracket id="bracket1" dir={true} />
-                                <PlayoffCard team1Score={playoff[4].team1Score} team2Score={playoff[4].team2Score} team1Logo={playoff[4].team1.logo} team2Logo={playoff[4].team2.logo} team1Name={playoff[4].team1.name} team2Name={playoff[4].team2.name} team1Id={playoff[4].team1.id} team2Id={playoff[4].team2.id} />
+                                <PlayoffCard time={playoff[4].startedAt} matchId={playoff[4].id} team1Score={playoff[4].team1Score} team2Score={playoff[4].team2Score} team1Logo={playoff[4].team1.logo} team2Logo={playoff[4].team2.logo} team1Name={playoff[4].team1.name} team2Name={playoff[4].team2.name} team1Id={playoff[4].team1.id} team2Id={playoff[4].team2.id} />
                             </div>
                             <div class="playoff-group" id="group6">
                                 <div class="playoff-group" id="group2">
-                                    <PlayoffCard team1Score={playoff[2].team1Score} team2Score={playoff[2].team2Score} team1Logo={playoff[2].team1.logo} team2Logo={playoff[2].team2.logo} team1Name={playoff[2].team1.name} team2Name={playoff[2].team2.name} team1Id={playoff[2].team1.id} team2Id={playoff[2].team2.id} />
-                                    <PlayoffCard team1Score={playoff[3].team1Score} team2Score={playoff[3].team2Score} team1Logo={playoff[3].team1.logo} team2Logo={playoff[3].team2.logo} team1Name={playoff[3].team1.name} team2Name={playoff[3].team2.name} team1Id={playoff[3].team1.id} team2Id={playoff[3].team2.id} />
+                                    <PlayoffCard time={playoff[2].startedAt} matchId={playoff[2].id} team1Score={playoff[2].team1Score} team2Score={playoff[2].team2Score} team1Logo={playoff[2].team1.logo} team2Logo={playoff[2].team2.logo} team1Name={playoff[2].team1.name} team2Name={playoff[2].team2.name} team1Id={playoff[2].team1.id} team2Id={playoff[2].team2.id} />
+                                    <PlayoffCard time={playoff[3].startedAt} matchId={playoff[3].id} team1Score={playoff[3].team1Score} team2Score={playoff[3].team2Score} team1Logo={playoff[3].team1.logo} team2Logo={playoff[3].team2.logo} team1Name={playoff[3].team1.name} team2Name={playoff[3].team2.name} team1Id={playoff[3].team1.id} team2Id={playoff[3].team2.id} />
                                 </div>
                                 <Bracket id="bracket2" dir={true} />
-                                <PlayoffCard team1Score={playoff[5].team1Score} team2Score={playoff[5].team2Score} team1Logo={playoff[5].team1.logo} team2Logo={playoff[5].team2.logo} team1Name={playoff[5].team1.name} team2Name={playoff[5].team2.name} team1Id={playoff[5].team1.id} team2Id={playoff[5].team2.id} />
+                                <PlayoffCard time={playoff[5].startedAt} matchId={playoff[5].id} team1Score={playoff[5].team1Score} team2Score={playoff[5].team2Score} team1Logo={playoff[5].team1.logo} team2Logo={playoff[5].team2.logo} team1Name={playoff[5].team1.name} team2Name={playoff[5].team2.name} team1Id={playoff[5].team1.id} team2Id={playoff[5].team2.id} />
                             </div>
                         </div>
                         <Bracket id="bracket3" dir={true} />
-                        <PlayoffCard team1Score={playoff[6].team1Score} team2Score={playoff[6].team2Score} team1Logo={playoff[6].team1.logo} team2Logo={playoff[6].team2.logo} team1Name={playoff[6].team1.name} team2Name={playoff[6].team2.name} team1Id={playoff[6].team1.id} team2Id={playoff[6].team2.id} />
+                        <PlayoffCard time={playoff[6].startedAt} matchId={playoff[6].id} team1Score={playoff[6].team1Score} team2Score={playoff[6].team2Score} team1Logo={playoff[6].team1.logo} team2Logo={playoff[6].team2.logo} team1Name={playoff[6].team1.name} team2Name={playoff[6].team2.name} team1Id={playoff[6].team1.id} team2Id={playoff[6].team2.id} />
                         <Follow id="follow1" />
-                        <PlayoffCard team1Score={playoff[6].team1Score} team2Score={playoff[6].team2Score} team1Logo={playoff[6].team1.logo} team2Logo={playoff[6].team2.logo} team1Name={playoff[6].team1.name} team2Name={playoff[6].team2.name} team1Id={playoff[6].team1.id} team2Id={playoff[6].team2.id} />
+                        <PlayoffCard time={playoff[9].startedAt} matchId={playoff[9].id} team1Score={playoff[9].team1Score} team2Score={playoff[9].team2Score} team1Logo={playoff[9].team1.logo} team2Logo={playoff[9].team2.logo} team1Name={playoff[9].team1.name} team2Name={playoff[9].team2.name} team1Id={playoff[9].team1.id} team2Id={playoff[9].team2.id} />
                     </div>
                     <div class="playoff-group" id="group4">
-                        <PlayoffCard team1Score={playoff[6].team1Score} team2Score={playoff[6].team2Score} team1Logo={playoff[6].team1.logo} team2Logo={playoff[6].team2.logo} team1Name={playoff[6].team1.name} team2Name={playoff[6].team2.name} team1Id={playoff[6].team1.id} team2Id={playoff[6].team2.id} />
+                        <PlayoffCard time={playoff[7].startedAt} matchId={playoff[7].id} team1Score={playoff[7].team1Score} team2Score={playoff[7].team2Score} team1Logo={playoff[7].team1.logo} team2Logo={playoff[7].team2.logo} team1Name={playoff[7].team1.name} team2Name={playoff[7].team2.name} team1Id={playoff[7].team1.id} team2Id={playoff[7].team2.id} />
                         <Line />
-                        <PlayoffCard team1Score={playoff[6].team1Score} team2Score={playoff[6].team2Score} team1Logo={playoff[6].team1.logo} team2Logo={playoff[6].team2.logo} team1Name={playoff[6].team1.name} team2Name={playoff[6].team2.name} team1Id={playoff[6].team1.id} team2Id={playoff[6].team2.id} />
+                        <PlayoffCard time={playoff[8].startedAt} matchId={playoff[8].id} team1Score={playoff[8].team1Score} team2Score={playoff[8].team2Score} team1Logo={playoff[8].team1.logo} team2Logo={playoff[8].team2.logo} team1Name={playoff[8].team1.name} team2Name={playoff[8].team2.name} team1Id={playoff[8].team1.id} team2Id={playoff[8].team2.id} />
                     </div>
                 </div>
-                
+
             </div>
         </div></>);
     } else {
